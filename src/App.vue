f63abb1d5d68fad1a97595ee7605a92a1ad22b91
Yabeth Villafuerte <soteloyabeth@gmail.com>
2023-11-14 05:01:21 -0600

    part2_vue.js3

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <script setup>
+import { ref } from 'vue';
 
-const listFruts = ['😎', '😊', '🤣', '❤️', '😁'];
-let seleccionMensaje = ''
-const mensaje1 = 'Hola'
-const mensaje2 = 'Buenos dias'
-const mensaje3 = 'Buenas noches'
-const mensaje4 = 'Me gusta VUE'
+const contador = ref(0)
 
-function funcionMensaje() {
-  if (seleccionMensaje === '1') {
-    alert(mensaje1)
-  } else if (seleccionMensaje === '2') {
-    alert(mensaje2)
-  } else if (seleccionMensaje === '3') {
-    alert(mensaje3)
-  } else if (seleccionMensaje === '4') {
-    alert(mensaje4)
-  }
+const reset = () =>{
+  contador.value = 0
 }
+
+const incrementa = () =>{
+  contador.value++;
+}
+const disminuye = () =>{
+  contador.value--;
+}
+
+
+
+
 
 
 
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li v-for="(frutas, index) in listFruts" :key="index">
-        número: <span>{{ index }}{{ frutas }}</span>
-      </li>
-    </ul>
+<h2 :class="contador > 0 ? 'positive' : 'negative' " > {{ contador }} </h2>
+<br>
+<button @click="incrementa">aumenta</button>
+<button @click="disminuye">disminuye</button>
+<button @click="reset">reset</button>
 
-    <div class="seleccion">
-      <label>Seleccione un mensaje a mostrar en pantalla</label>
-      <select v-model="seleccionMensaje" @change="funcionMensaje">
-        <option value="" disabled>Seleccione un mensaje</option>
-        <option value='1'>{{ mensaje1 }}</option>
-        <option value='2'>{{ mensaje2 }}</option>
-        <option value="3">{{ mensaje3 }}</option>
-        <option value="4">{{ mensaje4 }}</option>
-      </select>
-    </div>
-  </div>
+
 </template>
 
 
@@ -52,51 +40,15 @@ function funcionMensaje() {
   box-sizing: border-box;
 }
 
-
-
-h1 {
-  color: purple;
-  text-transform: uppercase;
-  font-weight: bold;
+.positive{
+  color: green;
 }
 
-li {
-  color: white;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-weight: bold;
-  font-size: 30px;
-  list-style: none;
-  background-color: rgb(106, 106, 106);
-  padding: 10px;
-  margin: 5px;
+.negative{
+  color: red;
 }
 
 
-.seleccion {
-  background-color: rgb(48, 48, 48);
-  height: 20vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  margin: 5px;
-}
-
-.seleccion label {
-  font-size: 20px;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-weight: bold;
-  padding: 10px;
-}
-
-.seleccion select {
-  padding: 4px 20px;
-  border-radius: 10px 10px 5px 5px;
-}
-
-.seleccion option {
-  font-size: 13px;
-}
 </style>
+
 
